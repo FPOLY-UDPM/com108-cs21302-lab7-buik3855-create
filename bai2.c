203d953dd59c8985c0631f5372d5c517c0a56ff4
buik3855-create <buik3855@gmail.com>
2026-02-28 14:28:21 +0700

    lap7

--- bai2.c
+++ bai2.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [bui dang khoa]
+ * MSSV:      [ps48500]
+ * Lớp:       [com108-cs21302]
  *****************************************************************************/
 
 //  BÀI 2: XÂY DỰNG CHƯƠNG TRÌNH ĐĂNG NHẬP BẰNG USERNAME VÀ PASSWORD 
@@ -9,4 +9,25 @@
 //  Output: Đăng nhập thành công hay không thành công
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <string.h>
+       
+int main()
+{
+    char username[20];
+    char password[20];
+
+    printf("Nhap username: ");
+    gets(username);
+
+    printf("Nhap password: ");
+    gets(password);
+
+    if(strcmp(username, "admin") == 0 && strcmp(password, "123456") == 0)
+        printf("Dang nhap thanh cong!");
+    else
+        printf("Dang nhap khong thanh cong!");
+
+    return 0;
+}
 
--- bai3.c
+++ bai3.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [bùi đăng khoa]
+ * MSSV:      [ps48500]
+ * Lớp:       [com108-cs21302]
  *****************************************************************************/
 
 //  BÀI 3: XÂY DỰNG CHƯƠNG TRÌNH SẮP XẾP CHUỖI THEO CHỮ CÁI 
@@ -9,4 +9,37 @@
 //  Output: Thứ tự các chuỗi đã được sắp xếp  
 
 // VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <string.h>
+ 
+int main()
+{
+    char s[5][100];
+    char temp[100];
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("Nhap chuoi thu %d: ", i+1);
+        gets(s[i]);
+    }
+
+    for(int i = 0; i < 4; i++)
+    {
+        for(int j = i + 1; j < 5; j++)
+        {
+            if(strcmp(s[i], s[j]) > 0)
+            {
+                strcpy(temp, s[i]);
+                strcpy(s[i], s[j]);
+                strcpy(s[j], temp);
+            }
+        }
+    }
+
+    printf("Cac chuoi sau khi sap xep:\n");
+    for(int i = 0; i < 5; i++)
+        printf("%s\n", s[i]);
+
+    return 0;
+}
 
